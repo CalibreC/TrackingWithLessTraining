19b96410c65ef17f8ce3205caea86930ca34436e
CalbireC <fd98shadow@sina.com>
2023-11-07 02:28:43 +0800

    modify split video

--- tools/split_video.py
+++ tools/split_video.py
@@ -1,14 +1,18 @@
 from pathlib import Path
+from typing import Union
 
 import cv2
 from tqdm import tqdm
 
 
-def show_video(path, cnt_max=900):
+def split_video(path: Path, cnt_max=900):
+    video_path = str(path / "test.mp4")
+    split_images_path = str(path / "split_images")
+
     # 获得视频的格式
-    video_capture = cv2.VideoCapture(path)
+    video_capture = cv2.VideoCapture(video_path)
     if not video_capture.isOpened():
-        print("Unable to open: " + path)
+        print("Unable to open: " + video_path)
         exit(0)
 
     fps = video_capture.get(cv2.CAP_PROP_FPS)
@@ -17,7 +21,7 @@ def show_video(path, cnt_max=900):
 
     for i in tqdm(range(cnt_max)):
         ret, frame = video_capture.read()
-        cv2.imwrite(f"../data/video/split_images/{i + 1}.jpg", frame)
+        cv2.imwrite(split_images_path + f"/{i + 1}.jpg", frame)
 
         if not ret:
             raise Exception("Video is too short!")
@@ -26,7 +30,10 @@ def show_video(path, cnt_max=900):
 
 
 if __name__ == "__main__":
-    base_path = Path("../data/video")
-    Path("../data/video/split_images").mkdir(parents=True, exist_ok=True)
+    video_data_path = Path("../data/video")
+    split_images_dir = video_data_path / "split_images"
+    test_video_path = video_data_path / "test.mp4"
+    if not split_images_dir.exists():
+        split_images_dir.mkdir(parents=True, exist_ok=True)
 
-    show_video("../data/video/test.mp4")
+    split_video(video_data_path)
